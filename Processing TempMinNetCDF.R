19ddccecf0eedb00c08014121ac46f9186e83a0c
marinalopez2110 <marinalopez2110@gmail.com>
2019-12-11 13:17:01 -0500

    Update Processing TempMinNetCDF.R
    Cleaned code

--- Processing TempMinNetCDF.R
+++ Processing TempMinNetCDF.R
@@ -17,6 +17,8 @@ library(ncdf.tools)
 ###Declaring filanmes and variables to explore####
 setwd("C:\\Users\\mlopez\\Documents\\GitHub\\Data Ouranos") #TELUQ
 fname <- "tasmin_day_CMCC-CMS_rcp45_r1i1p1_na10kgrid_qm-moving-50bins-detrend_1954.nc"
+RelVar <- "tasmin" #Relevant variable
+
 ncin <- nc_open(fname) #open netcdf file
 print(ncin)
 
@@ -31,7 +33,7 @@ nlat <- dim(lat)
 summary (lat)
 
 #Exploring temperature and time variables
-dunits <- ncatt_get(ncin,"tasmin","units")
+dunits <- ncatt_get(ncin, RelVar,"units")
 dunits
 tunits <- ncatt_get(ncin,"time","units")
 tunits
@@ -49,41 +51,25 @@ ncin$dim$time$calendar
 ###Convert into raster
 rasbrick <- brick(fname)
 rasbrick
-TempMin <- brick(fname, varname="tasmin", layer="time")
-str(TempMin)
+PlotVar <- brick(fname, varname=RelVar, layer="time")
+str(PlotVar)
 
 ###Create a time index for the multi-layer objetct
-TIME <- as.POSIXct(substr(TempMin@data@names, start = 2, stop=20), format="%Y.%m.%d")
+TIME <- as.POSIXct(substr(PlotVar@data@names, start = 2, stop=20), format="%Y.%m.%d")
 dftime <- data.frame(INDEX = 1:length(TIME), TIME=TIME)
 
 ###Create a subset of only the fist month of the year
 subset <- dftime[format(dftime$TIME, "%m") == "01",]
-
-sub.temp <- TempMin[[subset$INDEX]]
-
+sub.temp <- PlotVar[[subset$INDEX]]
 temp_month1 <- calc(sub.temp, fun=mean) #mean temperature of the month
 plot(temp_month1 - 273.15) #plot in Celsius for ONE MONTH
+
 ####### FINISHES PLOT MAP PER MONTH########
 
 
 ######## STARTS CODE FOR THE TIME SERIES PLOT##########
-# create dataframe -- reshape data
-# matrix (nlon*nlat rows by 2 cols) of lons and lats
-lonlat <- as.matrix(expand.grid(lon,lat))
-dim(lonlat)
-
-# vector of `tmp` values
-tmp_vec <- as.vector(tmp.slice)
-length(tmp_vec)
-
-# create dataframe and add names
-dname <- "tasmin"  # note: tmp means temperature (not temporary)
-tmp_df01 <- data.frame(cbind(lonlat,tmp_vec))
-names(tmp_df01) <- c("lon","lat",paste(dname,as.character(m), sep="_"))
-head(na.omit(tmp_df01), 10)
-
 # reshape the array into vector
-tmp_array <- ncvar_get(ncin,dname)
+tmp_array <- ncvar_get(ncin,RelVar)
 tmp_vec_long <- as.vector(tmp_array)
 length(tmp_vec_long)
 
@@ -96,10 +82,10 @@ head(na.omit(tmp_mat))
 # create a dataframe
 lonlat <- as.matrix(expand.grid(lon,lat))
 tmp_df02 <- data.frame(cbind(lonlat,tmp_mat))
-names(tmp_df02) <- c("lon","lat","tmpJan01","tmpJan02","tmpJan03","tmpJan04","tmpMay","tmpJun",
-                     "tmpJul","tmpAug","tmpSep","tmpOct","tmpNov","tmpDec")
+names(tmp_df02) <- c("lon","lat","tmpJan01","tmpJan02","tmpJan03","tmpJan04","tmpJan05","tmpJan06",
+                     "tmpJan07","tmpJan08","tmpJan09","tmpJan10","tmpJan11","tmpJan12")
 # Omit Invalid Values
-head(na.omit(tmp_df02, 20))
+head(na.omit(tmp_df02))
 ValidValuesTemp = na.omit(tmp_df02)
 ValidValuesTemp
 

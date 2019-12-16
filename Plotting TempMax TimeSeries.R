41cb38f7c3bf97374c1f4467380b11c8880e8817
marinalopez2110 <marinalopez2110@gmail.com>
2019-12-16 12:08:50 -0500

    Update Plotting TempMax TimeSeries.R
    Appended 3 files into an array, including time vector and slices of temperature

--- Plotting TempMax TimeSeries.R
+++ Plotting TempMax TimeSeries.R
@@ -26,14 +26,40 @@ path = "C:\\Users\\mlopez\\Documents\\GitHub\\Data Ouranos"
 #REGEX to find all files starting with the relevant vairable, any model, any rcp and any year:
 file.names <- dir(path, pattern = "^tasmax_day_[A-Za-z1-9-]+_rcp[0-9]+_r1i1p1_na10kgrid_qm-moving-50bins-detrend_[0-9]+[.]nc")
 file.names
+RelVar <- "tasmax" #Relevant variable
 
 #Loop to open all the netncd files of the variable that we care>
+var_array1 <- array(0, dim=c(1064,700,1096))
+time1 <- c()
+k=1
 for (i in 1:length(file.names)){
+  print ("Printing i")
+  print (i)
+  j <- i*365
+  print ("Printing j")
+  print (j)
   ncin <- nc_open(file.names[i]) #open netcdf file
   print(ncin)
+  dncin1 <- dim(ncin)
+  print ("Printing dimessions of ncin")
+  print (dncin1)
+  dimvar <- dim (ncvar_get(ncin,RelVar))
+  print ("Printing dimmesions of de variable tasmax on ncin")
+  print (dimvar)
+  var_array1[,,k:j] <- ncvar_get(ncin,RelVar)
+  dim(var_array1)
+  print ("Printing temperature array")
+  print (var_array1)
+  time1[k:j] <- ncvar_get(ncin,"time")
+  print ("Printing time")
+  print (time1)
+  k <- 1+j
+  print ("Printing k")
+  print (k)
+ 
 }
 
-RelVar <- "tasmax" #Relevant variable
+
 
 
 ######Starts Exploring and creating varibales#####
@@ -67,7 +93,8 @@ as.Date("1950-01-01") + 25915
 ######## STARTS CODE FOR THE TIME SERIES PLOT##########
 # reshape the array into vector
 var_array <- ncvar_get(ncin,RelVar)
-var_vec_long <- as.vector(var_array)
+dim(var_array1)
+var_vec_long <- as.vector(var_array1)
 length(var_vec_long)
 
 # reshape the vector into a matrix

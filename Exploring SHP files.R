53c3a324f0ff612b0fadc24f245c6489eb4b6d60
marinalopez2110 <marinalopez2110@gmail.com>
2019-12-17 12:23:01 -0500

    Update Exploring SHP files.R
    opening shapefiles with 3 different methods

--- Exploring SHP files.R
+++ Exploring SHP files.R
@@ -9,8 +9,19 @@ library(maptools)
 library(rgdal)
 library(shapefiles)
 
-
+#1 readShapePoly of the package maptools
 x <- CRS("+proj=longlat +ellps=WGS84")
 pa1 = readShapePoly("territoire_guide", verbose = TRUE, proj4string = x) 
 plot(pa1)
 pa1
+
+#2 readOGR
+pa2 = readOGR(".", "territoire_guide")
+
+#3 PBSMapping - it could distord data. Only use if any of the 2 previos don't work
+library(PBSmapping)
+pa3 = importShapefile("territoire_guide.shp")
+plot(pa3)
+
+#palutm <- spTransform(pa1, CRS("+proj=utm +zone=21 +ellps=WGS84")) #Gives error Error in .spTransform_Polygon(input[[i]], to_args = to_args, from_args = from_args,
+#Quebec zone between 17 and 21

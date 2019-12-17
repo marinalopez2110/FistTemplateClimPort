d3663d483f37e862e47b685b5a79ad2c3685b306
marinalopez2110 <marinalopez2110@gmail.com>
2019-12-17 12:03:33 -0500

    Update Exploring SHP files.R
    Reading shapefiles in R

--- Exploring SHP files.R
+++ Exploring SHP files.R
@@ -1,6 +1,7 @@
 ## READ IN SHAPEFILES
 
-setwd("H:\\Couches MFFP")
+#setwd("H:\\Couches MFFP") #Ouranos
+setwd("C:\\Users\\mlopez\\Documents\\GitHub\\Couches MFFP")  #TELUQ
 
 library(raster)
 library(rgeos)
@@ -8,3 +9,8 @@ library(maptools)
 library(rgdal)
 library(shapefiles)
 
+
+x <- CRS("+proj=longlat +ellps=WGS84")
+pa1 = readShapePoly("territoire_guide", verbose = TRUE, proj4string = x) 
+plot(pa1)
+pa1

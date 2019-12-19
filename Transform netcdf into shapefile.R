b87862c1080bb8984310094ff547e65f50028225
marinalopez2110 <marinalopez2110@gmail.com>
2019-12-19 12:06:47 -0500

    Create Transform netcdf into shapefile.R

---
+++ Transform netcdf into shapefile.R
@@ -0,0 +1,78 @@
+####TEMPERATURE ANALYSIS FILE (MAX AND MIN)
+
+library(readr)
+library(dplyr)
+library(tidyr)
+library(ggplot2)
+library(ggmap)
+library(viridis)
+library(weathermetrics)
+library(chron)
+library(RColorBrewer)
+library(lattice)
+library(ncdf4)
+library(maps)
+library(raster)
+library(PCICt)
+library(ncdf.tools)
+
+
+###Declaring filanmes and variables to explore####
+setwd("C:\\Users\\mlopez\\Documents\\GitHub\\Data Ouranos") #TELUQ
+
+fname <- "tasmin_day_CMCC-CMS_rcp45_r1i1p1_na10kgrid_qm-moving-50bins-detrend_1954.nc"
+RelVar <- "tasmin" #Relevant variable
+
+ncin <- nc_open(fname) #open netcdf file#print(ncin)
+print (ncin)
+
+######Starts Exploring and creating varibales#####
+### Longitude and Latitude varibales and dimenssions
+lon <- ncvar_get(ncin,"lon")
+nlon <- dim(lon)
+summary(lon)
+lat <- ncvar_get(ncin,"lat")
+nlat <- dim(lat)
+summary (lat)
+
+#Exploring Relevant variable (temp, prec) and time variables
+dunits <- ncatt_get(ncin, RelVar,"units")
+dunits
+tunits <- ncatt_get(ncin,"time","units")
+tunits
+time <- ncvar_get(ncin,"time")
+time
+nt <- dim(time)
+nt
+ncin$dim$time$units
+ncin$dim$time$calendar
+####Finishes Exploring and creating varibales#####
+
+
+
+####### STARTS PLOT MAP PER MONTH########
+###Convert into raster
+rasbrick <- brick(fname)
+rasbrick
+PlotVar <- brick(fname, varname=RelVar, layer="time")
+strPlotvar <- str(PlotVar)
+
+
+###Create a time index for the multi-layer objetct
+TIME <- as.POSIXct(substr(PlotVar@data@names, start = 2, stop=20), format="%Y.%m.%d")
+dftime <- data.frame(INDEX = 1:length(TIME), TIME=TIME)
+
+###Create a subset of only the fist month of the year
+subset <- dftime[format(dftime$TIME, "%m") == alist("01","02","03"),] #Selecting 3 months
+subset
+sub.var <- PlotVar[[subset$INDEX]]
+var_vec_long <- calc(sub.var, fun=mean) #mean variable(temp, prec) of the month
+plot(var_vec_long - 273.15) #plot in Celsius for THREE MONTH
+
+####### FINISHES PLOT MAP PER MONTH########
+
+
+
+
+nc_close(ncin) # Close netcdf file
+
